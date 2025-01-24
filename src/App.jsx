dcd66f391ebfe3466b262d61fe4ea6e8218e938f
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-01-24 11:40:54 +0530

    Material UI added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import '@material/web/button/filled-button.js';
+// import '@material/web/button/outlined-button.js';
+// import '@material/web/checkbox/checkbox.js';
+// import '@material/web/switch/switch.js';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <label>
+        Material 3
+        <md-checkbox checked></md-checkbox>
+      </label>
+
+      <md-outlined-button>Back</md-outlined-button>
+      <md-filled-button>Next</md-filled-button>
+      <br />
+      <md-switch icons></md-switch>
+
+      <md-switch icons show-only-selected-icon></md-switch>
+
+      <md-switch></md-switch> */}
     </>
   )
 }
