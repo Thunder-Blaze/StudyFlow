c05085d0ed923a360f46e04930b0e0ee78e5ac25
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-01-28 23:13:26 +0530

    Fixed Eslint Issues

--- src/components/FAQs/Questions_Answers.jsx
+++ src/components/FAQs/Questions_Answers.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 import './Questions_Answers.css'
 
 const Questions_Answers = (props) => {
-  return (
-    <div>
-      <div className="question">
-        <h1>Q. </h1>
-        <h2>{props.ques}</h2>
-      </div>
-      <div className="answer">
-      <h1>A. </h1>
-      <h2>{props.ans}</h2>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="question">
+                <h1>Q. </h1>
+                <h2>{props.ques}</h2>
+            </div>
+            <div className="answer">
+                <h1>A. </h1>
+                <h2>{props.ans}</h2>
+            </div>
+        </div>
+    )
+}
+
+Questions_Answers.propTypes = {
+    ques: PropTypes.number.isRequired,
+    ans: PropTypes.number.isRequired,
 }
 
 export default Questions_Answers
