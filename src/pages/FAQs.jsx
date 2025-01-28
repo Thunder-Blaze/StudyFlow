8eb63fe9347ae8282d32d1878bfbcaaf3393774d
V-Ajay-Chandra-24 <v.ajaychandra224199644@gmail.com>
2025-01-28 22:41:04 +0530

    Completed FAQs page

--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -1,10 +1,31 @@
 import Page_Layout from './Page_Layout'
+import './FAQs.css'
+import Questions_Answers from '../components/FAQs/Questions_Answers'
 
 const FAQs = () => {
     return (
         <Page_Layout>
-            <div className="p-4 pt-0 w-full h-full flex items-center justify-center flex-col gap-4">
-                <h1 className="text-3xl font-bold tracking-wider">FAQs</h1>
+            <div className="p-4 pt-0">
+                <h1 className="Heading">FAQs</h1>
+                <Questions_Answers
+                    ques="How do I create an account or log in for the first time?"
+                    ans="fdsjfsdjmf, sdjfsdn,f sdfnsfsdf snfsdmfnsdsf sdfnm,sd asjdajsk ajsfh aksjf jaskfjh aksfajsfh ajasfj hk"
+                />
+
+                <Questions_Answers
+                    ques="What should I do if I forget my password?"
+                    ans="fdsjfsdjmf, sdjfsdn,f sdfnsfsdf snfsdmfnsdsf sdfnm,sd"
+                />
+
+                <Questions_Answers
+                    ques="How can I update my personal information (e.g., email, phone number)?"
+                    ans="fdsjfsdjmf, sdjfsdn,f sdfnsfsdf snfsdmfnsdsf sdfnm,sd"
+                />
+
+                <Questions_Answers
+                    ques="How do I register for courses or drop a class?"
+                    ans="fdsjfsdjmf, sdjfsdn,f sdfnsfsdf snfsdmfnsdsf sdfnm,sd"
+                />
             </div>
         </Page_Layout>
     )
